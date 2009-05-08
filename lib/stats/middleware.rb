f54b342709aa1d3014f7eb082af2bf3f92cab283
Bryan Helmkamp <bryan@brynary.com>
2009-05-08 16:35:47 -0400

    Move memory usage logging to middleware

--- lib/stats/middleware.rb
+++ lib/stats/middleware.rb
@@ -8,20 +8,26 @@ class Stats
 
     def call(env)
       status, headers, body = nil
-      
+
       @stats.transaction do
         @stats[:uri] = env["PATH_INFO"]
         @stats[:http_method] = env["REQUEST_METHOD"]
-        
+
+        begin_memory = MemoryUsage.kilobytes
         status, headers, body = @app.call(env)
+        end_memory = MemoryUsage.kilobytes
+
+        @stats[:memory] = end_memory
+        @stats[:memory_delta] = end_memory - begin_memory
         
         if env["rack.routing_args"]
-          @stats[:controller_name] = env["rack.routing_args"]["controller"]
-          @stats[:action_name] = env["rack.routing_args"]["action"]
+          @stats[:controller_name]  = env["rack.routing_args"]["controller"]
+          @stats[:action_name]      = env["rack.routing_args"]["action"]
         end
+
         @stats[:response_code] = status
       end
-      
+
       [status, headers, body]
     end
 
